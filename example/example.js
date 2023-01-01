08d78fd352a45fb17de1b82ab35327558a8ca327
unknown <dzmitry.duboyski@gmail.com>
2023-01-01 19:09:41 +0400

    refactor: update example.js

--- example/example.js
+++ example/example.js
@@ -10,17 +10,22 @@ const getfakeUser = require('../index')
 // }
 
 
-console.log('First example:')
+console.log("First example - getfakeUser() :")
 console.log(getfakeUser())
+console.log('\n')
 
-console.log('Second example:')
+console.log("Second example - getfakeUser({count: 3, gender: 'male'}) :")
 console.log(getfakeUser({count: 3, gender: 'male'}))
+console.log('\n')
 
-console.log('Third example:')
+console.log("Third example - getfakeUser({count: 1, language: 'en'}) :")
 console.log(getfakeUser({count: 1, language: 'en'}))
+console.log('\n')
 
-console.log('Fourth example:')
+console.log("Fourth example - getfakeUser({language: 'ru'}) :")
 console.log(getfakeUser({language: 'ru'}))
+console.log('\n')
 
-console.log('Fifth example:')
+console.log("Fifth example - getfakeUser({language: 'uk'}) :")
 console.log(getfakeUser({language: 'uk'}))
+console.log('\n')
