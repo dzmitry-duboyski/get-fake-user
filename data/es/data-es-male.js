fb4f4e1192a219fee7592fc8c37a43a6dc43974d
unknown <dzmitry.duboyski@gmail.com>
2023-01-05 16:31:50 +0400

    feat: init Spanish language

---
+++ data/es/data-es-male.js
@@ -0,0 +1,17 @@
+
+/**
+ * Here is the data that is used when creating the user.
+ * 
+ * source for firstName:
+ * source for lastName:
+ */
+module.exports = {
+  'firstNameArray': [
+    " ",
+  ],
+  'lastNameArray': [
+    "",
+ ]
+}
+
+
